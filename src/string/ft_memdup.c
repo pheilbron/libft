651aa5befb64fd180df3a73bad5e5237609ded53
pheilbro <pheilbro@student.42.us.org>
2019-11-20 14:04:10 -0800

    Fix norm errors.

--- src/string/ft_memdup.c
+++ src/string/ft_memdup.c
@@ -6,11 +6,11 @@
 /*   By: pheilbro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 15:09:54 by pheilbro          #+#    #+#             */
-/*   Updated: 2019/10/25 15:11:59 by pheilbro         ###   ########.fr       */
+/*   Updated: 2019/11/20 14:03:39 by pheilbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_string.h"
+#include "ft_string.h"
 
 void	*ft_memdup(const void *src, size_t size)
 {
